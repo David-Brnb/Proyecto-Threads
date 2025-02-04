b64b8ebc7ff013e99a5ac2a1e6eaaa7f529a49c9
David-Brnb <321171197@ciencias.unam.mx>
2025-02-04 17:55:23 -0600

    add: finish

--- l/PrimosHilos.cpp
+++ l/PrimosHilos.cpp
@@ -1,16 +1,16 @@
-// // =================================================================
-// //
-// // File: PrimosHilos.cpp
-// // Author: David Bernabe
-// // Description: This file implements the adding all prime numbers in a segment of numbers
-// //		using C/C++ threads. To compile:
-// //		g++ -o app PrimosHilos.cpp
-// //
-// // Copyright (c) 2024 by Tecnologico de Monterrey.
-// // All Rights Reserved. May be reproduced for any non-commercial
-// // purpose.
-// //
-// // =================================================================
+// =================================================================
+//
+// File: SumaPares.cpp
+// Author: David Bernabe
+// Description: This file implements the counting of pair numbers in an array
+//		using C/C++ threads. To compile:
+//		g++ -o app SumaPares.cpp
+//
+// Copyright (c) 2024 by Tecnologico de Monterrey.
+// All Rights Reserved. May be reproduced for any non-commercial
+// purpose.
+//
+// =================================================================
 
 #include <iostream>
 #include <iomanip>
@@ -21,12 +21,13 @@
 using namespace std;
 using namespace std::chrono;
 
-#define SIZE    5e6 // 5e9 o 5 millones 
+#define SIZE   5000000 // 1e9
+#define THREADS std::thread::hardware_concurrency()
 
-void getSum(long long &sum, int size) {
+void getSum(int start, int end, long long &sum) {
     sum=0;
 
-    for (int i = 0; i < size; i++) {
+    for (int i = start; i < end; i++) {
         if(i<2){
             continue;
         }
@@ -38,20 +39,35 @@ void getSum(long long &sum, int size) {
 
         if(res) sum+=i;
     }
-
 }
 
 int main(int argc, char* argv[]) {
+
     // These variables are used to keep track of the execution time.
     high_resolution_clock::time_point startTime, endTime;
     double timeElapsed;
 
+    int end, blockSize;
+    thread threads[THREADS];
+    long long result[THREADS];
+    blockSize = SIZE / THREADS;
+
     cout << "Starting...\n";
+    timeElapsed = 0;
     long long totalSum;
     for (int j = 0; j < N; j++) {
         startTime = high_resolution_clock::now();
-        
-        getSum(totalSum, SIZE);
+
+        totalSum=0;
+        for (int i = 0; i < THREADS; i++) {
+            end = (i != (THREADS - 1))? (i + 1) * blockSize : SIZE;
+            threads[i] = thread(getSum, (i * blockSize), end, std::ref(result[i]));
+        }
+
+        for (int i = 0; i < THREADS; i++) {
+            threads[i].join();
+            totalSum+=result[i];
+        }
         cout << totalSum << endl;
 
         endTime = high_resolution_clock::now();
